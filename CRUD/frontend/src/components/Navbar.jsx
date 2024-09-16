26ed56eb8c86c8145d342e0dc8024b7099e3d705
JulesBestDev176 <souleymanefall176@gmail.com>
2024-09-16 23:28:43 +0000

    create product

--- CRUD/frontend/src/components/Navbar.jsx
+++ CRUD/frontend/src/components/Navbar.jsx
@@ -4,53 +4,54 @@ import { Link } from "react-router-dom"
 import { CiSquarePlus } from "react-icons/ci"
 import { IoMoon } from "react-icons/io5"
 import { LuSun } from "react-icons/lu"
+import { useProductStore } from '../store/product';
 
 const Navbar = () => {
 
-    const {colorMode, toggleColorMode} = useColorMode();
+    const { colorMode, toggleColorMode } = useColorMode();
+    const { products } = useProductStore();
+    return (
+        <Container
+            maxW={"1140px"}
+            px={4}
 
-  return (
-  <Container 
-    maxW={"1140px"}
-    px={4}
-    
-    >
-        <Flex
-            h={16}
-            alignItems={"center"}
-            justifyContent={"space-between"}
-            flex={{
-                base:"column",
-                sm:"row"
-            }}
         >
-            <Text
-                fontSize={{ base:"22", sm:"28"}}
-                fontWeight={'bold'}
-                textTransform={"uppercase"}
-                textAlign={"center"}
-                bgGradient={'linear(to-r, cyan.400, blue.500)'}
-                bgClip='text'
+            <Flex
+                h={16}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                flex={{
+                    base: "column",
+                    sm: "row"
+                }}
             >
-                <Link to={"/"}>Product Store 🛒</Link>
-            </Text>
+                <Text
+                    fontSize={{ base: "22", sm: "28" }}
+                    fontWeight={'bold'}
+                    textTransform={"uppercase"}
+                    textAlign={"center"}
+                    bgGradient={'linear(to-r, cyan.400, blue.500)'}
+                    bgClip='text'
+                >
+                    <Link to={"/"}>Product Store 🛒</Link>
+                </Text>
 
-            <HStack spacing={2} alignItems={"Center"}>
-                <Link to={"/create"}>
-                    <Button>
-                        <CiSquarePlus fontSize={20}/>
+                <HStack spacing={2} alignItems={"Center"}>
+                    <Link to={"/create"}>
+                        <Button>
+                            <CiSquarePlus fontSize={20} />
+                        </Button>
+                    </Link>
+                    <Button onClick={toggleColorMode}>
+                        {colorMode === "light" ? <IoMoon /> : <LuSun />}
                     </Button>
-                </Link>
-                <Button onClick={toggleColorMode}>
-                    {colorMode === "light" ? <IoMoon /> : <LuSun />   }
-                </Button>
 
-            </HStack>
+                </HStack>
 
-        </Flex>
+            </Flex>
 
-  </Container>
-  )
+        </Container>
+    )
 }
 
 export default Navbar
